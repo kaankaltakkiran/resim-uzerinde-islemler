81dc7db3516a76910342f789a6bf9862be23d3cc
kaankalttakiran <durdu.kaan.kaltakkiran@gmail.com>
2022-06-14 16:28:44 +0300

    Görüntü İşleme İşlemleri
    resim üzerinde işlemler

---
+++ resim üzerinde işlemler/deneme6/deneme6/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿
+using System;
+
+namespace deneme6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(418, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Resim Yükle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::deneme6.Properties.Resources.doğa;
+            this.pictureBox1.Location = new System.Drawing.Point(50, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(394, 171);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 62);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Büyüt +";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(531, 171);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 62);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Küçült -";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(64, 390);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 27);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(288, 390);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(77, 44);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "İşlem";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(138, 349);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Oransal Katsayı";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(499, 299);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(483, 292);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(85, 27);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(609, 292);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(85, 27);
+            this.textBox3.TabIndex = 9;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(558, 337);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(77, 44);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Yukarı";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(558, 406);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(77, 44);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Aşşağı";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(458, 373);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(77, 44);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "Sol";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(641, 373);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(77, 44);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "Sağ";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(881, 102);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(77, 44);
+            this.button9.TabIndex = 14;
+            this.button9.Text = "90°";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(881, 201);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(77, 44);
+            this.button10.TabIndex = 15;
+            this.button10.Text = "180°";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(881, 287);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(77, 44);
+            this.button11.TabIndex = 16;
+            this.button11.Text = "270°";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(0, 0);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 19;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(881, 12);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(77, 44);
+            this.button13.TabIndex = 18;
+            this.button13.Text = "0°";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(881, 381);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(77, 44);
+            this.button14.TabIndex = 20;
+            this.button14.Text = "360°";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(575, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(300, 68);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Durdu Kaan Kaltakkıran 201913172065";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1098, 504);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Hafta 7 Ödev";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Button11_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Button13_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ resim üzerinde işlemler/deneme6/deneme6/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace deneme6
+{
+    public partial class Form1 : Form
+    {
+        Bitmap resim;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           if (openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                resim = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = resim;
+            }
+            Graphics.FromImage(resim).DrawImage(pictureBox1.Image, 0, 0, 300, 300);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Width += 10;
+            pictureBox1.Height += 10;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Width -= 10;
+            pictureBox1.Height -= 10;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double w, h, k;
+            k = Convert.ToDouble(textBox1.Text);
+            w = pictureBox1.Width;
+            h = pictureBox1.Height;
+
+            h = h * k;
+            w = w * k;
+            pictureBox1.Width = Convert.ToInt32(w);
+            pictureBox1.Height = Convert.ToInt32(h);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int x, y;
+            x = pictureBox1.Location.X;
+            y = pictureBox1.Location.Y;
+
+            y -= 10;
+            pictureBox1.Location = new Point(x, y);
+            textBox2.Text = Convert.ToString(x);
+            textBox3.Text = Convert.ToString(y);
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int x, y;
+            x = pictureBox1.Location.X;
+            y = pictureBox1.Location.Y;
+
+            x -= 10;
+            pictureBox1.Location = new Point(x, y);
+            textBox2.Text = Convert.ToString(x);
+            textBox3.Text = Convert.ToString(y);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int x, y;
+            x = pictureBox1.Location.X;
+            y = pictureBox1.Location.Y;
+
+            y += 10;
+            pictureBox1.Location = new Point(x, y);
+            textBox2.Text = Convert.ToString(x);
+            textBox3.Text = Convert.ToString(y);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int x, y;
+            x = pictureBox1.Location.X;
+            y = pictureBox1.Location.Y;
+
+            x += 10;
+            pictureBox1.Location = new Point(x, y);
+            textBox2.Text = Convert.ToString(x);
+            textBox3.Text = Convert.ToString(y);
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+          //  pictureBox1.Image.RotateFlip(RotateFlipType.);
+           // pictureBox1.Refresh();
+           
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            pictureBox1.Refresh();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            pictureBox1.Refresh();
+        }
+        private void button11_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            pictureBox1.Refresh();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            // pictureBox1.Image.RotateFlip(RotateFlipType.RotateNoneFlipXY);
+            //   pictureBox1.Refresh();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+             static Bitmap RotateImage(Image image, float rotateAtX, float rotateAtY, float angle, bool bNoClip)
+            {
+                int W, H, X, Y;
+                if (bNoClip)
+                {
+                    double dW = (double)image.Width;
+                    double dH = (double)image.Height;
+
+                    double degrees = Math.Abs(angle);
+                    if (degrees <= 90)
+                    {
+                        double radians = 0.0174532925 * degrees;
+                        double dSin = Math.Sin(radians);
+                        double dCos = Math.Cos(radians);
+                        W = (int)(dH * dSin + dW * dCos);
+                        H = (int)(dW * dSin + dH * dCos);
+                        X = (W - image.Width) / 2;
+                        Y = (H - image.Height) / 2;
+                    }
+                    else
+                    {
+                        degrees -= 90;
+                        double radians = 0.0174532925 * degrees;
+                        double dSin = Math.Sin(radians);
+                        double dCos = Math.Cos(radians);
+                        W = (int)(dW * dSin + dH * dCos);
+                        H = (int)(dH * dSin + dW * dCos);
+                        X = (W - image.Width) / 2;
+                        Y = (H - image.Height) / 2;
+                    }
+                }
+                else
+                {
+                    W = image.Width;
+                    H = image.Height;
+                    X = 0;
+                    Y = 0;
+                }
+
+                //create a new empty bitmap to hold rotated image
+                Bitmap bmpRet = new Bitmap(W, H);
+                bmpRet.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+                //make a graphics object from the empty bitmap
+                Graphics g = Graphics.FromImage(bmpRet);
+
+                //Put the rotation point in the "center" of the image
+                g.TranslateTransform(rotateAtX + X, rotateAtY + Y);
+
+                //rotate the image
+                g.RotateTransform(angle);
+
+                //move the image back
+                g.TranslateTransform(-rotateAtX - X, -rotateAtY - Y);
+
+                //draw passed in image onto graphics object
+                g.DrawImage(image, new PointF(0 + X, 0 + Y));
+
+                return bmpRet;
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+           // textBox4.Text = "Durdu Kaan Kaltakkıran 201913172065";
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
